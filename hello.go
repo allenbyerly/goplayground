7b2595d49cfe22a23813d9c1bb967a8c7bac6546
Allen Byerly <me@allenbyerly.com>
2017-05-06 12:21:29 -0700

    added random number line

--- hello.go
+++ hello.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
   fmt.Println("Hello World!")
-  fmt.Println("This is my Go Playground")
-  fmt.Println("The time is", time.Now())
+  fmt.Println("This is my Go Playground!")
+  fmt.Println("The time is: ", time.Now())
+  fmt.Println("MA random number is: ", rand.Intn(10))
 }
