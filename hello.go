7f51826bc8daa1c4948157a4dd3564057a438ddb
Allen Byerly <me@allenbyerly.com>
2017-05-06 12:33:31 -0700

    Added comments to explain the components of hello.go

--- hello.go
+++ hello.go
@@ -1,13 +1,31 @@
+//This is the hello world exaple in go. It displays a hello world prompt.
+//In addition it displays prompts to demonstrate some basic functions.
+
+//Author: Allen Byerly
+//Name: hello
+
+//The Main Package Definition
 package main
 
+//The Imported Libraries
 import (
+  //import format library
   "fmt"
+  //import time library
   "time"
+  //import random number functions from the math library
+  "math/rand"
   )
 
+//Main Package Function Definition
 func main() {
+  //Print a Name Prompt
   fmt.Println("Hello World!")
+  //Print a Description Prompt
   fmt.Println("This is my Go Playground!")
+  //Print a current time Prompt
   fmt.Println("The time is: ", time.Now())
+  //Print a random number prompt
+  //note: the random number will always return the same number until it is seeded
   fmt.Println("MA random number is: ", rand.Intn(10))
 }
